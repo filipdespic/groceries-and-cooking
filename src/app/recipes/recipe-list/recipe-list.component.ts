9ea389127660f7f98c52a2996e17d3fb746fa035
filipdespic <despicpb12@gmail.com>
2024-02-09 11:52:24 +0100

    add recipe list and recipe description templates

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test desc', 'https://www.eatingwell.com/thmb/0RZlB0S2IgjqYHjFFXUPMxpYT3w=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/chickpea-curry-chhole-1x1-41ea4d53c7df4fddabd83caa5b57718e.jpg  '),
+    new Recipe('Another Recipe', 'This is simply another test desc', 'https://www.eatingwell.com/thmb/0RZlB0S2IgjqYHjFFXUPMxpYT3w=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/chickpea-curry-chhole-1x1-41ea4d53c7df4fddabd83caa5b57718e.jpg')
+  ];
 
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
 }
