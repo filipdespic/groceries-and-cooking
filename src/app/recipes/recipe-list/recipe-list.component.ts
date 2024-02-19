43d7e5c33ae41d6b9922edf01576a6f15f05d79e
filipdespic <despicpb12@gmail.com>
2024-02-19 11:05:23 +0100

    add recipes to recipe-item component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,8 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent {
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test desc', 'https://www.eatingwell.com/thmb/0RZlB0S2IgjqYHjFFXUPMxpYT3w=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/chickpea-curry-chhole-1x1-41ea4d53c7df4fddabd83caa5b57718e.jpg  '),
-    new Recipe('Another Recipe', 'This is simply another test desc', 'https://www.eatingwell.com/thmb/0RZlB0S2IgjqYHjFFXUPMxpYT3w=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/chickpea-curry-chhole-1x1-41ea4d53c7df4fddabd83caa5b57718e.jpg')
+    new Recipe('Another Recipe', 'This is simply another test desc', 'https://www.eatingwell.com/thmb/0RZlB0S2IgjqYHjFFXUPMxpYT3w=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/chickpea-curry-chhole-1x1-41ea4d53c7df4fddabd83caa5b57718e.jpg'),
+    new Recipe('Test', 'This is simply another test desc', 'https://www.eatingwell.com/thmb/0RZlB0S2IgjqYHjFFXUPMxpYT3w=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/chickpea-curry-chhole-1x1-41ea4d53c7df4fddabd83caa5b57718e.jpg')
   ];
 
   constructor() {
